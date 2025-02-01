59b0e6cc23b0d2346ce1fe0a90f91f4c984aee90
Nebojsa Knezevic <global021man@gmail.com>
2025-02-01 08:17:54 +0100

    Game logic added and finished project.

--- src/components/hangman-word/hangman-word.tsx
+++ src/components/hangman-word/hangman-word.tsx
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-export const HangmanWord: React.FC = () => {
+interface IHangmanWord {
+    word: string;
+    guessedLetters: string[];
+    gameState: string;
+    setResetRef: React.Dispatch<React.SetStateAction<() => void>>;
+}
 
-    const word = "test"
-    const guessedLetter = ['t', 'r', 'e']
+export const HangmanWord: React.FC<IHangmanWord> = ({ word, guessedLetters, gameState, setResetRef }) => {
+    const myRef = useRef<(HTMLSpanElement | null)[]>([]);
+
     const wordStyle: React.CSSProperties = {
-        display: 'flex',
-        gap:'.25rem',
-        fontSize: '6rem',
-        fontWeight: 'bold',
-        textTransform: 'uppercase',
-        fontFamily: 'monospace'
+        display: "flex",
+        gap: ".25rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+    };
+
+    const resetRef = (): void => {
+        myRef.current.forEach((el) => {
+            if (el) {
+                el.style.visibility = "hidden";
+                el.style.color = "black";
+            }
+        });
     }
 
-    return(<div style={wordStyle}>
-        {word
-        .split("")
-        .map((item, index)=>{
-            return(
-                <span style={{borderBottom: '.1em solid black'}} key={index}>
-                    <span style={{visibility: guessedLetter.includes(item) ? 
-                        'visible' : 'hidden'}}>{item}</span>
-                </span>
-            );
-        })}
-    </div>);
-}
+    useEffect(() => {
+        setResetRef(() => resetRef);
+    }, [setResetRef]);
+
+    useEffect(() => {
+        if (gameState === "lose") {
+            myRef.current.forEach((el) => {
+                if (el && el.style.visibility === "hidden") {
+                    el.style.visibility = "visible";
+                    el.style.color = "red";
+                }
+            });
+        }
+    }, [gameState]);
+
+
+
+    return (
+        <div style={wordStyle}>
+            {word.split("").map((item, index) => {
+                const isVisible = guessedLetters.includes(item.toLowerCase());
+
+                return (
+                    <span key={index} style={{ borderBottom: ".1em solid black" }}>
+                        <span
+                            ref={(el) => (myRef.current[index] = el)}
+                            style={{
+                                color: "black",
+                                visibility: isVisible ? "visible" : "hidden",
+                            }}
+                        >
+                            {item}
+                        </span>
+                    </span>
+                );
+            })}
+        </div>
+    );
+};
--- src/components/keyboard/keyboard.tsx
+++ src/components/keyboard/keyboard.tsx
@@ -1,5 +1,6 @@
 import React from "react";  
 import styles from './keyboard.module.css'
+import { GameState } from "../../App";
 
 const englishLetters = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
@@ -7,8 +8,19 @@ const englishLetters = [
     'W', 'X', 'Y', 'Z'
   ];
   
+interface IKeyboard{
+    setGuessedLetters: React.Dispatch<React.SetStateAction<string[]>>;
+    guessedLetters: {
+        positive: string[],
+        negative: string[]
+    },
+    gameState: string
+    // setGameState: React.Dispatch<React.SetStateAction<GameState>>
+}
 
-export const Keyboard: React.FC = () => {
+export const Keyboard: React.FC<IKeyboard> = (props) => {
+    const{setGuessedLetters, guessedLetters, gameState}=props
+
 
     const keyboardStyle: React.CSSProperties = {
         display: 'grid',
@@ -16,10 +28,28 @@ export const Keyboard: React.FC = () => {
         gap: '.5rem'
     }
 
+    const getButtonClass = (letter: string) => {
+        if (guessedLetters.positive.includes(letter.toLowerCase())) {
+          return `${styles.btn} ${styles.active}`;
+        }
+
+        if (guessedLetters.negative.includes(letter.toLowerCase())) {
+          return `${styles.btn} ${styles.inactive}`;
+        }
+
+        if (gameState != 'playing') {
+            return `${styles.btn} ${styles.inactive}`;
+        }
+        
+        return styles.btn;
+      };
+
     return(
         <div style={keyboardStyle}>
             {englishLetters.map((item, index)=>{
-                return(<button className={styles.btn} key={item}>{item}</button>);
+                return(<button  className={getButtonClass(item)} key={item} 
+                disabled={guessedLetters.positive.includes(item.toLocaleLowerCase()) || guessedLetters.negative.includes(item.toLocaleLowerCase())}
+                onClick={()=>setGuessedLetters(prev => [...prev, item.toLocaleLowerCase()])}>{item}</button>);
             })}
         </div>
     );
